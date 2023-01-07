331174ffd05c5131d2191e5459eb554bcc8adb30
LauraMayock <mcfadden.laura@yahoo.com>
2023-01-07 16:22:06 +0000

    Added home app and templates

--- leather_works/settings.py
+++ leather_works/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
 ]
 
@@ -61,7 +62,10 @@ ROOT_URLCONF = 'leather_works.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
