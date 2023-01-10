d4b2a7e151d9391afec4a3905f5aa8051a882b46
LauraMayock <mcfadden.laura@yahoo.com>
2023-01-10 11:28:16 +0000

    Update admin

---
+++ products/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CateroryAdmin)
