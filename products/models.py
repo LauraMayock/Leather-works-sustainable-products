f64adeab8c584cbaffcb351d6c4b3cfa3e458feb
LauraMayock <mcfadden.laura@yahoo.com>
2023-01-11 20:08:00 +0000

    style product detail page

--- products/models.py
+++ products/models.py
@@ -23,6 +23,10 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_url_2 = models.URLField(max_length=1024, null=True, blank=True)
+    image_url_3 = models.URLField(max_length=1024, null=True, blank=True)
+    thumbnail_url = models.URLField(max_length=1024, null=True, blank=True)
     thumbnail = models.ImageField(null=True, blank=True)
 
     def __str__(self):
