078e76c8213324cf9e9367f3799f13df40671115
LauraMayock <mcfadden.laura@yahoo.com>
2023-01-30 20:45:24 +0000

    Add stripe and style

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,7 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form,
+        'order_form': order_form,  
     }
 
     return render(request, template, context)
