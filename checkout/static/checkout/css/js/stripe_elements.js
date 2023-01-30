b6d14dd29e2560012f653c229b0e34f0ecb19fcd
LauraMayock <mcfadden.laura@yahoo.com>
2023-01-30 21:34:04 +0000

    add checkout functionality

--- checkout/static/checkout/css/js/stripe_elements.js
+++ checkout/static/checkout/css/js/stripe_elements.js
@@ -7,13 +7,10 @@
 
 */
 
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
-let client_secret = $('#id_client_secret').text().slice(1,-1);
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1,-1);
+let clientSecret = $('#id_client_secret').text().slice(1,-1);
 let stripe = Stripe(stripe_public_key);
 let elements = stripe.elements();
-let card = elements.create('card');
-card.mount('#card-element');
-
 let style = {
     base: {
         color: '#000',
@@ -31,3 +28,49 @@ let style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+//Handle realtime validation errors on the card element
+card.addEventListener('change', function (event){
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+        <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    } 
+})
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- leather_works/settings.py
+++ leather_works/settings.py
@@ -175,6 +175,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 FREE_DELIVERY = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
+#Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
