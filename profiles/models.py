26b82d4e43c5792fb6fca98d138ee73d5b29344c
LauraMayock <mcfadden.laura@yahoo.com>
2023-02-04 17:58:52 +0000

    update allauth templates

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class UserProfile(models.Model):
     """
     Customer profile, containing customer information and order histiory
     """
-    customer = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label="Country *", null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
@@ -30,6 +30,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
-    instance.Userprofile.save()
+    instance.userprofile.save()
 
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
+
 
 def profile(request):
     """
     Display user profile
     """
-
+    profile = get_object_or_404(UserProf, user=request.user)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
