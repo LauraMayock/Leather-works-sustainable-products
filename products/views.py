fde1802b8a5e16a2711b34c44110aafee4c6325a
LauraMayock <mcfadden.laura@yahoo.com>
2023-02-06 13:25:38 +0000

    add a Delete product function for superusers

--- products/views.py
+++ products/views.py
@@ -75,9 +75,9 @@ def add_product(request):
     if request.method == 'POST':
         form = NewProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -111,3 +111,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete product from the website"""
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
