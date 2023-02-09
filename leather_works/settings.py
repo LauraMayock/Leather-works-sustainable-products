fea943cfd1b3858178bff13ebe88f6abce76a0de
LauraMayock <mcfadden.laura@yahoo.com>
2023-02-09 17:05:11 +0000

    Amend typo in settings

--- leather_works/settings.py
+++ leather_works/settings.py
@@ -184,9 +184,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Cache control
+     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT'
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
 
